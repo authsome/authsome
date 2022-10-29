957de9cdb82827a0614bffda712cc9d6e62c4c92
jeffgat <jeff.gtbn@gmail.com>
2022-10-29 10:20:51 -0700

    Added tailwind in properly

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
